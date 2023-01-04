3af68181a112f6e66e34f36f4a46c6c3a66bb967
sabrighiloufi <51623850+sabrighiloufi@users.noreply.github.com>
2023-01-04 11:57:08 +0100

    Update authGoogle.js

--- passport_strategies/authGoogle.js
+++ passport_strategies/authGoogle.js
@@ -1,8 +1,8 @@
 
 const passport = require("passport")
 const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
-const GOOGLE_CLIENT_ID = '148237496069-r8e3e02j0bktukebblj9g5afh90ndmfh.apps.googleusercontent.com'
-const GOOGLE_CLIENT_SECRET = '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'
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
 
 passport.use(new GoogleStrategy({
     clientID:     GOOGLE_CLIENT_ID,
